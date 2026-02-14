79100e71dc25995516531619b033ed6bb2615f1f
Youssefihab185 <yhame@uic.edu>
2026-02-13 22:05:20 -0600

    kk

--- youssef.cpp
+++ youssef.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 using namespace std;
 
+// Rank professors based on thier RatemyProfesoor rating 
+// Case 1: same rating -> pick the one with more votes
+// Case 2: same number of votes: pick the one with the least difficulty 
+// Case 3: Same difficulty: pick a random one and put the other one after 
 
 int main(){
-
-
-    cout << "Hello YOUSSEF" << endl;
+    
+    cout << "Hello" << endl;
 
     return 0;
 }
