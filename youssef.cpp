d70b54c583d3b3313e64ff3f7b4436b1a9d5d18b
Youssefihab185 <yhame@uic.edu>
2026-02-13 22:15:36 -0600

    Project functionality comments

--- youssef.cpp
+++ youssef.cpp
@@ -7,7 +7,7 @@ int main(){
 // Case 1: same rating -> pick the one with more votes
 // Case 2: same number of votes: pick the one with the least difficulty 
 // Case 3: Same difficulty: pick a random one and put the other one after 
-    
+
     cout << "Hello" << endl;
 
     return 0;
